1de96edbc366384f8fc59a5be2754fb529891383
shubhpro <shubhpro@shubhams-MacBook-Pro.local>
2022-09-07 18:14:32 +0530

    fixes

---
+++ server.js
@@ -0,0 +1,41 @@
+const express=require("express");
+const app=express();
+const path=require("path");
+const ErrorMiddleware = require("./middlewares/commonErrorHandler");
+
+let adharData=[{
+    name:"Rajkumar",
+    adhar:101
+  },
+            {
+    name:"Sweta",
+    adhar:102
+  },
+            {
+    name:"Girish",
+    adhar:103
+  },
+            {
+    name:"Pranjali",
+    adhar:104
+  }
+           
+           
+           ]
+
+app.get("/getAdharAndNames",function(req,res){
+
+    res.json({
+        message:"Success",
+        data:adharData
+    })
+
+})
+
+
+
+app.use(ErrorMiddleware)
+
+app.listen(8080,function(){
+    console.log(`Server Running `);
+})
